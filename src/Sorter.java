3a7dbc1bc1f1c521ef41052337955767e0c8b631
Saerath <saerath13@gmail.com>
2019-02-15 10:48:11 +0300

    Fix selectSort

--- src/Sorter.java
+++ src/Sorter.java
@@ -6,17 +6,26 @@ class Sorter {
 		
 		long time = System.nanoTime()/1000000;
 		
-		int tempInd;
+		Integer temp, min;
+		int minInd;
 		
 		for (int i = 0; i<list.size(); i++){
+			
+			minInd = i;
+			min = list.get(i);
+			
 			for (int j = i+1; j < list.size(); j++) {
-				if (((Integer)list.get(j)).intValue() < ((Integer)list.get(i)).intValue()) {
-					tempInd = j;
-					list.set(j, list.get(i));
-					list.set(i, list.get(tempInd));
+				if (((Integer)list.get(j)).intValue() < min) {
+					minInd = j;
+					min = list.get(j);
 				}
 			}
+
+			list.set(minInd, list.get(i));
+			list.set(i, min);
+
 		}
+		
 		
 		System.out.printf("Sorting time = %d ms\n", System.nanoTime()/1000000 - time);
 		
@@ -32,6 +41,22 @@ class Sorter {
 			for (int j = i-1; (j >= 0) && (x < list.get(j)); j--) {
 				list.set(j+1, list.get(j));
 				list.set(j, x);
+			}
+			
+		}
+
+		System.out.printf("Sorting time = %d ms\n", System.nanoTime()/1000000 - time);
+		
+		return list;
+	}
+	
+	public ArrayList<Integer> bubbleSort(ArrayList<Integer> list, boolean writeTime){
+		long time = System.nanoTime()/1000000;
+		
+		for (int i = 0; i < list.size(); i++) {
+			
+			for (int j = i; j < list.size(); j++) {
+				
 			}
 			
 		}
