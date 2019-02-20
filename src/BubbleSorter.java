b312260af2abd867e4fab82f56ec1ebefd62b442
Saerath <saerath13@gmail.com>
2019-02-20 08:12:44 +0300

    Method Sort in BubbleSorter changed.

--- src/BubbleSorter.java
+++ src/BubbleSorter.java
@@ -1,27 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
 
-
-public class BubbleSorter implements Sorter{
+public class BubbleSorter implements Sorter {
 
 	public List<Integer> sort(List<Integer> list, boolean writeTime) {
-		long time = System.nanoTime()/1000000;
-		
+		long time = System.nanoTime() / 1000000;
+		List<Integer> listNew = new ArrayList<Integer>();
+		listNew = list;
 		Integer temp;
-		
-		for (int i = 0; i < list.size(); i++) {
-			
-			for (int j = list.size()-1; j > i; j--) {
-				if (list.get(j) < list.get(j-1)) {
-					temp = list.get(j);
-					list.set(j, list.get(j-1));
-					list.set(j-1, temp);
+
+		for (int i = 0; i < listNew.size(); i++) {
+
+			for (int j = listNew.size() - 1; j > i; j--) {
+				if (listNew.get(j) < listNew.get(j - 1)) {
+					temp = listNew.get(j);
+					listNew.set(j, listNew.get(j - 1));
+					listNew.set(j - 1, temp);
 				}
 			}
 		}
 
-		System.out.printf("BubbleSort, Sorting time = %d ms\n", System.nanoTime()/1000000 - time);
-		
-		return list;
+		if (writeTime) {
+			System.out.printf("BubbleSort, Sorting time = %d ms\n",
+					System.nanoTime() / 1000000 - time);
+		}
+
+		return listNew;
 	}
 
 }
