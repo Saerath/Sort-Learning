7fdc29041b33d54bc012e8fc298e88dcd12b9551
Saerath <saerath13@gmail.com>
2019-02-20 11:29:17 +0300

    All sorts in main.java

--- src/main.java
+++ src/main.java
@@ -2,24 +2,30 @@ import java.util.*;
 
 public class main {
 
-	final static int ARR_LENGTH = 10;
+	final static int ARR_LENGTH = 10000;
 	final static int MAX_NUMBER = 50000;
 	final static boolean WRITE_TIME = true;
 
 	public static void main(String[] args) {
 
-		System.out.printf("Array length = %d,\nMax element of Array = %d\n",
-				ARR_LENGTH, MAX_NUMBER);
+//		System.out.printf("Array length = %d,\nMax element of Array = %d\n",
+//				ARR_LENGTH, MAX_NUMBER);
 
 		// build Array with ARR_LENGTH and MAX_NUMBER for each index
 		RandomArrayGenerator arrBuilder = new RandomArrayGenerator();
-		Sorter sorter = SortNames.BUBBLE.getSorter();
+		Sorter sorterBubble = SortNames.BUBBLE.getSorter();
+		Sorter sorterInsert = SortNames.INSERT.getSorter();
+		Sorter sorterSelect = SortNames.SELECT.getSorter();
+		Sorter sorterShaker = SortNames.SHAKER.getSorter();
 
 		ArrayList<Integer> list = arrBuilder.newArray(ARR_LENGTH, MAX_NUMBER);
-
-		sorter.sort(list, WRITE_TIME);
+		ArrayList<Integer> listNew = new ArrayList<Integer>(list);
 
 		System.out.println(list);
+		sorterBubble.sort(listNew, WRITE_TIME);
+		sorterInsert.sort(listNew, WRITE_TIME);
+		sorterSelect.sort(listNew, WRITE_TIME);
+		sorterShaker.sort(listNew, WRITE_TIME);
 
 	}
 
